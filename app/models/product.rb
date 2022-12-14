2ee6e64053ea47c79221daca6625826fb0591aeb
mamuncvasu <mamunsikder@cvasu.ac.bd>
2022-12-14 13:30:40 +0600

    Cart Update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
@@ -7,8 +10,7 @@ class Product < ApplicationRecord
 	}
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
-	has_many :line_items
-	before_destroy :ensure_not_referenced_by_any_line_item
+
 
 	def ensure_not_referenced_by_any_line_item
 		unless line_items.empty?
