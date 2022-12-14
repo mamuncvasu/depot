b388e9a0dca819378298ab6556c82d264450d3b6
mamuncvasu <mamunsikder@cvasu.ac.bd>
2022-12-15 00:10:16 +0600

    smart cart completed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,7 @@ class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /line_items or /line_items.json
   def index
@@ -24,12 +25,12 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    # @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        # format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        # format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -53,10 +54,10 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1 or /line_items/1.json
   def destroy
-    @line_item.destroy
-
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +70,11 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
+    end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
     end
 end
